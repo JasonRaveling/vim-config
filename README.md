9c4e2b39c52673daa8bd87a6f96b4d29be23e2e3
Jason Raveling <jason@webunraveling.com>
2018-03-23 21:34:22 -0500

    Reinitialization of vim7 repo
    Some servers are still running Vim 7 so we're going to keep these Vim 7 configs alive... ALIVE!

--- README.md
+++ README.md
@@ -1,14 +1,22 @@
-These configs are aimed at being used in the terminal for web development (PHP, JS, CSS, SASS). Although, it should work with pretty much any languages. After all its Vim. While these configs are meant for terminal they should work well with gVim too.
+These configs are aimed at being used in the terminal for web development (PHP, JS, CSS, SASS). Although, it should work with pretty much any languages. Afterall its Vim. While these configs are meant for terminal they should work well with gVim too.
+
+The color schemes, plugins, etc. all have the files and content taken directly from their repos or source for easy updating from the original source when needed.
 
 ## Installation
 These configs were made using GNU/Linux but may work on other operating systems. To install, just open the terminal and do the following.
 
-1. `git clone https://github.com/webunraveling/vim-config.git`
-1. Rename this repos directory to `.vim` (be sure to make a copy of your original as a backup)
-1. Create a new `.vimrc` in your home directory with one line in it: `source $HOME/.vim/vimrc`
+`git clone https://github.com/webunraveling/vim-config.git && cd vim-config`
+
+Then choose if you'd like to install automatically or manually.
+
+### Automatic
+`chmod +x install.sh && ./install.sh`
+
+### Manual
+Just place all of the directories from this repository into your `.vim` directory. If you already have a `.vim` directory then place the contents into their respective directories. Then place `vimrc` in your home directory, renamed to `.vimrc` (a dot file). Although, creating symlinks would be a better option than copying files as you can just run `git pull` when the repo is updated and there will be no need to copy all of the files again.
 
 ## Adding Color Schemes
-This is completely optional. If your XTERM doesn't already use 256 colors, add `export TERM=xterm-256color` to your `.bashrc`.
+This is completely optional. If you already have color schemes be sure you aren't overwritting anything before running this command. If your XTERM doesn't already use 256 colors, add `export TERM=xterm-256color` to your `.bashrc`. This is done for you if you use the automatic install script.
 
-# Please submit any emacs rants as follows
+# do not display errors
 `echo $emacsRant > /dev/null`
--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-# This is a small script that will modify your bashrc and vimrc.
+# A small script that will install all symlinks for you. Use at your own risk. This will create symlinks in the respective vim directories in your home directory.
 
 # setup vars
 vimDir=$HOME/.vim;
@@ -6,32 +6,37 @@ vimrc=$HOME/.vimrc;
 bundleDir=$HOME/.vim/bundle # dir that pathogen loads from
 scriptDir=`dirname $(readlink -f $0)`;
 
-# warning message before proceeding
-echo "BEFORE YOU BEGIN: Be sure you have backed up your .vim directory and moved/renamed the vim-config directory (this repo) to .vim. Otherwise the configs will not work."
-echo
-read -p "Have you done this already? (y/N) " readyOrNot
-if [[ $readyOrNot != "y" && $readyOrNot != "Y" ]]; then
-    echo "Exited script."
-    echo
-    exit;
+# create .vim directory if it doesnt exist
+if [ ! -d $vimDir ]; then
+    echo ".vim does not exist. Creating it.";
+    mkdir ${HOME}/.vim;
 fi
 
-#########################
-# changes to .vimrc
-#########################
-
-# new source for .vimrc
+# create link for .vimrc
 if [ -f $vimrc ]; then
-    mv $vimrc $HOME/.vimrcBACKUP-`date +%s`;
-    echo '.vimrc already exists... backed up to .vimrcBACKUP-'`date +%s`;
+    echo '.vimrc already exists... backed up to .vimrcBACKUP';
+    mv $vimrc $HOME/.vimrcBACKUP;
 fi
 
-echo 'source $HOME/.vim/vimrc' > $vimrc;
+ln -s $PWD/vimrc $vimrc;
 
 
-#########################
-# changes to .bashrc
-#########################
+# create link for color schemes
+########################################
+if [ ! -d $vimDir/colors ]; then
+    echo "Creating 'colors' directory: ${vimDir}/colors";
+    mkdir "${vimDir}/colors";
+fi
+
+for s in `ls colors/*`
+do
+    colorLink="${vimDir}/colors/"`basename $s`;
+
+    if [ ! -e $colorLink ]; then
+        echo "Adding color scheme: ${s}";
+        ln -s "${PWD}/${s}" $colorLink;
+    fi
+done
 
 # set 256 color in terminal
 if ! grep -q "export TERM=xterm-256color" $HOME/.bashrc; then 
@@ -39,10 +44,118 @@ if ! grep -q "export TERM=xterm-256color" $HOME/.bashrc; then
     echo 'export TERM=xterm-256color' >> $HOME/.bashrc;
 fi
 
-# set solarized colors for everything in the terminal
-if ! grep -q "dircolors.256dark" $HOME/.bashrc; then
-    echo 'Adding Solarized Dark support to .bashrc.';
+# set solarized colors for all other of terminal
+if ! grep -q "eval `dircolors $scriptDir/misc/dir_colors/dircolors.256dark`" $HOME/.bashrc; then
+    echo 'Adding Solarized Dark support for other terminal applications.';
     echo 'eval `dircolors '$scriptDir'/misc/dir_colors/dircolors.256dark`' >> $HOME/.bashrc;
-else
-    echo 'dircolors.256color already present in .bashrc... skipping.'
 fi
+
+# auto load solarized theme in Vim
+if [ ! -d ${vimDir}/autoload ]; then
+    echo "Creating directory: ${vimDir}/autoload";
+    mkdir ${vimDir}/autoload;
+fi
+
+ln -s $PWD/autoload $vimDir/autoload;
+
+# add color schemes to Konsole
+if [ ! -d $HOME/.kde/share/apps/konsole ]; then
+    echo;
+    echo "Konsole configs were not found. Konsole is KDE's terminal application. If you do not use Konsole, you can answer \"N\" but you should probably add the needed color scheme to your terminal application so that Vim color schemes appear as intended."; 
+    echo;
+    read -p "Would you like to copy the color scheme files for Konsole (${HOME}/.kde/share/apps/konsole/) [Y/n] " answer;
+
+    if [[ $answer == "Y" || $answer == "y" || -z $answer ]]; then
+        mkdir -p $HOME/.kde/share/apps/konsole;
+    fi
+fi
+
+for ks in `ls misc/konsole/*`
+do
+    konsoleColor="${HOME}/.kde/share/apps/konsole/"`basename $ks`;
+
+    if [ ! `basename $ks` == "readme.md" ]; then
+        echo "Adding Konsole color scheme: ${ks}";
+        ln -s "${PWD}/${ks}" $konsoleColor;
+    fi
+done
+
+# find out if we should create Konsole colorscheme files
+
+
+# add indentation
+########################################
+if [ ! -d $vimDir/indent ]; then
+    echo "Creating directory: ${vimDir}/indent";
+    mkdir ${vimDir}/indent;
+fi
+
+for ind in `ls indent/*`;
+do
+    indentLink="${vimDir}/indent/"`basename $ind`;
+
+    if [ ! -e $indentLink ]; then
+        echo "Adding indent script: ${ind}";
+        ln -s "${PWD}/${ind}" $indentLink;
+    fi
+done
+
+# add syntax
+########################################
+
+if [ ! -d $vimDir/syntax ]; then
+    mkdir $vimDir/syntax;
+fi
+
+for syn in `ls syntax/*`;
+do
+    syntaxLink="${vimDir}/syntax/"`basename $syn`;
+
+    if [ ! -e $syntaxLink ]; then
+        echo "Adding syntax script: ${syn}";
+        ln -s "${PWD}/${syn}" $syntaxLink; 
+    fi
+done
+
+
+# add plugins
+########################################
+if [ ! -d $vimDir/plugins ]; then
+    echo "Creating directory: ${vimDir}/plugins";
+    mkdir $vimDir/plugins;
+fi
+
+# add pathogen (handles the rest of the plugins)
+if [ ! -d $vimDir/autoload ]; then
+    echo "Creating 'autoload' directory: ${vimDir}/autoload";
+    mkdir $vimDir/autoload;
+fi
+
+echo "Adding Pathogen Plugin Manager";
+if [ ! -e $vimDir/autoload/pathogen.vim ]; then
+    ln -s $PWD/plugins/vim-pathogen/autoload/pathogen.vim "${vimDir}/autoload/pathogen.vim";
+fi
+
+if [ ! -d $bundleDir ]; then
+    echo "Created directory: ${bundle}";
+    mkdir $bundleDir;
+fi
+
+# add NERD tree
+if [ ! -e $bundleDir/nerdtree ]; then
+    echo "Adding Plugin: NERD Tree";
+    ln -s $PWD/plugins/nerdtree $bundleDir/;
+fi
+
+# add vim-css3-syntax
+if [ ! -e $bundleDir/vim-css3-syntax ]; then
+    echo "Adding Plugin: vim-css3-syntax";
+    ln -s $PWD/plugins/vim-css3-syntax $bundleDir/;
+fi
+
+# add vim-gitgutter
+if [ ! -e $bundleDir/vim-gitgutter ]; then
+    echo "Adding Plugin: vim-gitgutter";
+    ln -s $PWD/plugins/vim-gitgutter $bundleDir/;
+fi
+
